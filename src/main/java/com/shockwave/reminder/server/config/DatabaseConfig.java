7b38def4ca0a62bf0ef174c252623f6c1b53f892
Roman Petrov <dev.petrov.roman@gmail.com>
2017-02-24 16:34:31 +0300

    Started configure datasource for work with database.

---
+++ src/main/java/com/shockwave/reminder/server/config/DatabaseConfig.java
@@ -0,0 +1,69 @@
+package com.shockwave.reminder.server.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories("com.shockwave.reminder.server.repository")
+@EnableTransactionManagement
+@ComponentScan("com.shockwave.reminder.server")
+@PropertySource("classpath:db.properties")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(environment.getRequiredProperty("db.entity.package"));
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(getHibernateProperties());
+
+
+
+
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
+
+    public Properties getHibernateProperties() {
+        Properties properties = null;
+        try {
+            properties = new Properties();
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(inputStream);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+}
