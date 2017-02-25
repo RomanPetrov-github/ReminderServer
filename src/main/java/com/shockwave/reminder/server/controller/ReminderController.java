e58968657866f35e33b38ee07aae0f02f60c827c
Roman Petrov <dev.petrov.roman@gmail.com>
2017-02-25 15:16:34 +0300

    Configure REST services.

--- src/main/java/com/shockwave/reminder/server/controller/ReminderController.java
+++ src/main/java/com/shockwave/reminder/server/controller/ReminderController.java
@@ -1,18 +1,28 @@
 package com.shockwave.reminder.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.shockwave.reminder.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap modelMap){
-        return "My Reminder";
+    public Remind getReminder(){
+        return getMockRemind();
+    }
+
+    private Remind getMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindTitle("My first Remind");
+        remind.setRemindDate(new Date());
+        return remind;
     }
 }
